9ec356df83ca10e71ea013dd3f0115f15637027e
hmb <mstfhll353@gmail.com>
2023-09-18 19:04:32 +0300

    Homework 4 added

---
+++ Ders 7/Homework-4/Homework-4/PageX.swift
@@ -0,0 +1,19 @@
+//
+//  PageX.swift
+//  Homework-4
+//
+//  Created by Musti on 18.09.2023.
+//
+
+import UIKit
+
+class PageX: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        // Do any additional setup after loading the view.
+    }
+    
+    
+}
