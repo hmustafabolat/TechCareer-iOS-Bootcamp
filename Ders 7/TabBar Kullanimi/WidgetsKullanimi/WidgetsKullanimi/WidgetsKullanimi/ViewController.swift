3d6bc7985034d241fcd786ca9e500cfedaead28e
hmb <mstfhll353@gmail.com>
2023-09-19 19:54:27 +0300

    Lesson 7 devam

--- Ders 7/TabBar Kullanimi/WidgetsKullanimi/WidgetsKullanimi/WidgetsKullanimi/ViewController.swift
+++ Ders 7/TabBar Kullanimi/WidgetsKullanimi/WidgetsKullanimi/WidgetsKullanimi/ViewController.swift
@@ -19,6 +19,18 @@ class ViewController: UIViewController {
     
     @IBOutlet weak var labelSwitch: UILabel!
     
+    @IBOutlet weak var segmentedControl: UISegmentedControl!
+    
+    @IBOutlet weak var labelSegmented: UILabel!
+    
+    @IBOutlet weak var slider: UISlider!
+    
+    @IBOutlet weak var labelSlider: UILabel!
+    
+    @IBOutlet weak var stepper: UIStepper!
+    
+    @IBOutlet weak var labelStepper: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         if mSwitch.isOn{
@@ -26,6 +38,10 @@ class ViewController: UIViewController {
         }else{
             labelSwitch.text = "Kapalı"
         }
+        
+        let secilenIndeks = segmentedControl.selectedSegmentIndex
+        let secilenKategori = segmentedControl.titleForSegment(at: secilenIndeks)
+        labelSegmented.text = "Seçim: \(secilenKategori!)"
     }
 
     @IBAction func buttonMutlu(_ sender: Any) {
@@ -42,9 +58,28 @@ class ViewController: UIViewController {
         }
     }
     
+    @IBAction func segmentedDegisim(_ sender: UISegmentedControl) {
+        let secilenIndeks = sender.selectedSegmentIndex
+        let secilenKategori = sender.titleForSegment(at: secilenIndeks)
+        labelSegmented.text = "Seçim: \(secilenKategori!)"
+    }
+    
+    @IBAction func sliderDegisim(_ sender: UISlider) {
+        labelSlider.text = String(Int(sender.value))
+    }
+    
+    @IBAction func stepperDegisim(_ sender: Any) {
+    }
+    
     @IBAction func buttonGoster(_ sender: Any) {
         print("Switch Durum: \(mSwitch.isOn)")
+        let secilenIndeks = segmentedControl.selectedSegmentIndex
+        let secilenKategori = segmentedControl.titleForSegment(at: secilenIndeks)
+        print("Seçilen : \(secilenIndeks)")
+        print("Seçilen : \(secilenKategori!)")
+        print("Slider : \(slider.value)")
     }
+    
     @IBAction func switchDegisim(_ sender: UISwitch) {
         if sender.isOn{
             labelSwitch.text = "Açık"
@@ -52,6 +87,8 @@ class ViewController: UIViewController {
             labelSwitch.text = "Kapalı"
         }
     }
+    
+    
     
     
 }
