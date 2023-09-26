2b1f20c423a9813c93ed815bee5b425f37bd3712
hmb <mstfhll353@gmail.com>
2023-09-26 18:39:58 +0300

    Lesson 10 Urunler app added

---
+++ Ders 10/Ürünler App/UrunlerApp/UrunlerApp/Urunler.swift
@@ -0,0 +1,8 @@
+//
+//  Urunler.swift
+//  UrunlerApp
+//
+//  Created by Musti on 26.09.2023.
+//
+
+import Foundation
---
+++ Ders 10/Ürünler App/UrunlerApp/UrunlerApp/UrunlerHucre.swift
@@ -0,0 +1,23 @@
+//
+//  UrunlerHucre.swift
+//  UrunlerApp
+//
+//  Created by Musti on 26.09.2023.
+//
+
+import UIKit
+
+class UrunlerHucre: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
