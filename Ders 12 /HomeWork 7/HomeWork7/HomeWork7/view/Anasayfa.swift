aebad2c3e078ac533e511420b0700d100a81ef6e
hmb <mstfhll353@gmail.com>
2023-10-03 01:26:34 +0300

    HomeWork7 started

---
+++ Ders 12 /HomeWork 7/HomeWork7/HomeWork7/view/Anasayfa.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  HomeWork7
+//
+//  Created by Musti on 3.10.2023.
+//
+
+import UIKit
+
+class Anasayfa: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ Ders 12 /HomeWork 7/HomeWork7/HomeWork7/view/DetaySayfa.swift
@@ -0,0 +1,17 @@
+//
+//  DetaySayfa.swift
+//  HomeWork7
+//
+//  Created by Musti on 3.10.2023.
+//
+
+import UIKit
+
+class DetaySayfa: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        
+    }
+}
